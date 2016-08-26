11b2eaea288b817a6b03c70655d198af1fa5c959
yhb852948970 <kevin07yhb@gmail.com>
2016-08-26 14:48:59 +0800

    send to ST

--- README.md
+++ README.md
@@ -1,5 +1,41 @@
 # UAV-phase1
 
-This code is for the UAV navigation in the urban environment based on stereo vision.
+ROS stack for the NTU-ST UAV autonomous navigation project. Phase 1 deliverable.
 
-This are three modules, i.e. ueye camera driver, stereo matching, depth to laser scan conversion.
+
+In this ROS stack, there are three modules, i.e. ueye, stereo_disparity, depth_to_line_extraction.
+
+1. ueye package is the camera driver.
+
+2. stereo_disparity module is in charge of the stereo matching part.
+
+3. depth_to_line_extraction is used to convert the depth message to a ROS laser_scan message.
+
+4. ROS rviz tool is used for visulization.  
+
+
+
+Dependency of this stack:
+
+1. Ueye camera official driver, which is provided by IDS and it can be downloaded from this website.
+
+2. iriutils package. This is the driver for IDS ueye camera in ROS.
+
+3. OpenCV 2.4.x. x can be 8-13. For our testing, OpenCV 2.4.12 and OpenCV 2.4.13 are used.
+
+4. ROS indigo. 
+
+5. These algorithms are tested in Ubuntu Linux 14.04.
+
+
+
+Usage of this stack:
+
+1. put the fold "UAV-phase1" into the catkin workspace named "catkin_ws" in the home directory. 
+If the catkin workspace are not named like this or in the other directory, please amend the path to rviz file in the launch file accordingly.
+
+2. catkin_make
+
+3. roslaunch /path/to/launch/file/ST_UAV.launch
+
+A demo launch file using the data in a rosbag is provided. The name of that launch file is "ST_UAV_demo.launch". In the demo, rosbag is required, but the stereo cameras are not necessary. 
--- stereo_disparity/src/stereo_disparity.cpp
+++ stereo_disparity/src/stereo_disparity.cpp
@@ -7,11 +7,11 @@
 #include <tf/transform_broadcaster.h>
 
 // OpenCV headers
-#include "opencv2/core/core.hpp"
-#include "opencv2/calib3d/calib3d.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/contrib/contrib.hpp"
+#include <opencv2/core/core.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/contrib/contrib.hpp>
 
 // system headers
 #include <math.h>
@@ -19,19 +19,11 @@
 #include <algorithm>
 #include <stdio.h>
 
-#define width 752
-#define height 480
+#define WIDTH 752
+#define HEIGHT 480
 
 using namespace std;
 using namespace cv;
-/*
-double M_1[9] = { 420.6975, 0, 368.2764, 0, 418.8214, 239.7958, 0, 0, 1  };
-double M_2[9] = { 424.2353, 0, 376.1645, 0, 422.5504, 237.5235, 0, 0, 1  };
-double D_1[4] = {  -0.2930, 0.0839, -9.8567e-04, 3.0856e-03  };
-double D_2[4] = {  -0.2991, 0.0916, -1.6000e-03, 5.4858e-04  };
-double r_data[9] = { 0.9998, -0.0051, 0.0199, 0.0051, 1.0000, -9.5538e-05, -0.0199, 1.9647e-04, 0.9998};
-double t_data[3] = { -2.995894e+02, -0.7549, 1.9833 };
-*/
 
 double r_data[9] = {0.999851516004708, -0.00498457045275044, 0.0164954539340047, 0.00497160703876063, 0.999987299688415, 0.000826792189086883, -0.0164993656405163, -0.000744660508793523, 0.999863598904464};
 double t_data[3] = {-299.268590358311,	-0.641667329520781,	1.09821941809761};
@@ -70,14 +62,13 @@ class stereo_disparity
 
   StereoSGBM sgbm;
   int cn;
-  float depth;
 
   public:
   stereo_disparity() : 	it(nh) {
-	roi_left = Rect(0, 0, width, height);
-	roi_right = Rect(width, 0, width, height);
-	roi_half = Rect(0, height/4, width, height/2);
-	img_size = Size(width, height);
+	roi_left = Rect(0, 0, WIDTH, HEIGHT);
+	roi_right = Rect(WIDTH, 0, WIDTH, HEIGHT);
+	roi_half = Rect(0, HEIGHT/4, WIDTH, HEIGHT/2);
+	img_size = Size(WIDTH, HEIGHT);
 
     	M1= cv::Mat(3, 3, CV_64FC1, &M_1);
     	M2 = cv::Mat(3, 3, CV_64FC1, &M_2);
@@ -87,27 +78,21 @@ class stereo_disparity
     	T = cv::Mat(3, 1, CV_64FC1, &t_data);
 
 	cn = 1;
-	sgbm.preFilterCap = 63; //34
-	sgbm.SADWindowSize = 5; //1
-	sgbm.P1 = 20*cn*sgbm.SADWindowSize*sgbm.SADWindowSize;
+	sgbm.preFilterCap = 63; 
+	sgbm.SADWindowSize = 5; 
+	sgbm.P1 = 10*cn*sgbm.SADWindowSize*sgbm.SADWindowSize;
 	sgbm.P2 = 32*cn*sgbm.SADWindowSize*sgbm.SADWindowSize;
-  	sgbm.disp12MaxDiff = 1; // cannot set it to 0
+  	sgbm.disp12MaxDiff = 1; 
 	sgbm.minDisparity = 3;
 	sgbm.numberOfDisparities = 64;
 	sgbm.uniquenessRatio = 10;
 	sgbm.speckleWindowSize = 125;
-	sgbm.speckleRange = 1; // will check this param
-
-	//sgbm.fullDP = alg == STEREO_SGBM;
-
-	depth =0;
+	sgbm.speckleRange = 1; 
 
 	stereoRectify( M1, D1, M2, D2, img_size, R, T, R1, R2, P1, P2, Q, CALIB_ZERO_DISPARITY, 0, img_size, &roi1, &roi2 );
 	initUndistortRectifyMap(M1, D1, R1, P1, img_size, CV_16SC2, map11, map12);
 	initUndistortRectifyMap(M2, D2, R2, P2, img_size, CV_16SC2, map21, map22);
-cout << "Q = " << Q << endl << endl;
-cout << "P1 = " << P1 << endl << endl;
-cout << "P2 = " << P2 << endl << endl;
+
   	img_combine = it.subscribe("/camera/image_combine", 1, &stereo_disparity::imageCallback, this);
 	img_disparity = it.advertise("/stereo/disparity", 1);
 	img_depth = it.advertise("/stereo/depth", 1);
@@ -138,20 +123,12 @@ cout << "P2 = " << P2 << endl << endl;
 	img_left_half = img_left_rect(roi_half);
 	img_right_half = img_right_rect(roi_half);
 
-	int64 t = getTickCount();
 	sgbm(img_left_half, img_right_half, disp);
 	disp.convertTo(disp8, CV_8U, 255/(sgbm.numberOfDisparities*16.));
 	disp_ori = disp / 16;
 	reprojectImageTo3D(disp_ori, depth_32F, Q);
 	split(depth_32F, bgr);
 	bgr[2] = bgr[2] / 1000;
-//	depth = disp.at<short>(height/4,width/2);
-//	depth = 420*0.3*16/disp.at<short>(disp.rows/2, disp.cols/2);
-	depth = bgr[2].at<float>(height/4,width/2);
-
-	ROS_INFO_STREAM("depth of the center point is " << depth << " m." );
-	t = getTickCount() - t;
-	ROS_INFO_STREAM("Stereo Matching frequency: " << getTickFrequency()/t);
 
 	sensor_msgs::ImagePtr disparity = cv_bridge::CvImage(std_msgs::Header(), "mono8", disp8).toImageMsg();
         img_disparity.publish(disparity);
@@ -163,7 +140,7 @@ cout << "P2 = " << P2 << endl << endl;
 
 int main(int argc, char** argv){
 
-	ros::init(argc, argv, "stereo_disparity");	// The third argument is the cpp filename
+	ros::init(argc, argv, "stereo_disparity");
 	stereo_disparity stereo;
 	ros::spin();
 	return 0;
